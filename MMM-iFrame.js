21a2563c01ccd74f20395c06cecba7e13f4e1cde
Albert Wong <atwong@alumni.uci.edu>
2017-06-13 18:24:23 -0700

    adding resume()

--- MMM-iFrame.js
+++ MMM-iFrame.js
@@ -13,29 +13,48 @@ Module.register("MMM-iFrame",{
 				height:"300px",
 				width:"100%",
                                 updateInterval: 0.5 * 60 * 1000,
-                                url: ["http://magicmirror.builders/"]
+                                url: ["http://magicmirror.builders/"],
+                                scrolling: "no"
 		},
 
         start: function () {
                 self = this;
-                setInterval( function () { 
+                var count = 0;
+                if (this.config.url.length > 1 ) {
+                      setInterval( function () { 
                          self.updateDom(1000);
-                         console.log('update')
+                         console.log('update' + count++)
                          }, this.config.updateInterval);
+                }
 	},
   getRandomInt: function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   },
+resume: function() {
+   console.log("Resuming");
+   return this.getDom();
+},
+
         // Override dom generator.
 	getDom: function() {
 		var iframe = document.createElement("IFRAME");
 		iframe.style = "border:0"
 		iframe.width = this.config.width;
 		iframe.height = this.config.height;
-                var url_index = this.getRandomInt(0,this.config.url.length);
-                console.log("URL length:" + this.config.url.length + " " + "URL index:" + url_index);
-                iframe.src = this.config.url[url_index];
-                console.log("url:" + iframe.src);
+                iframe.scrolling = this.config.scrolling;
+                var url_index = 0;
+         //       console.log("currentURL:" + this.currentURL);
+                var repeat = true;
+                while(repeat) {
+                    url_index = this.getRandomInt(0,this.config.url.length);
+                    futureURL = this.config.url[url_index];
+                    console.log("URL_length:" + this.config.url.length + " " + "URL_index:" + url_index + " " + "url:" + futureURL);
+           //         if( futureURL == currentURL) {
+                        iframe.src = futureURL;
+             //           currentURL = futureURL;
+                        repeat = false;
+               //     } 
+                }
 		return iframe;
 	}
 
